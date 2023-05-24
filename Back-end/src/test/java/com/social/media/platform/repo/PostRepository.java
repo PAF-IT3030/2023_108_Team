53a705ae8b8940d391cdd0d45c2654c2353b7cc1
geeth eshan <98637377+GeethEshan@users.noreply.github.com>
2023-05-24 23:35:13 +0530

    Delete Back-end/src/test/java/com/social/media/platform/repo directory

--- Back-end/src/test/java/com/social/media/platform/repo/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.social.media.platform.repo;
-
-import com.social.media.platform.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-}
