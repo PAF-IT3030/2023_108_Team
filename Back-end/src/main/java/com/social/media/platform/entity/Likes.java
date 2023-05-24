5a6a224d47e9535c0aab99ef80175c03174a3bd6
dilshandevinda <101094478+dilshancomz@users.noreply.github.com>
2023-05-24 23:40:51 +0530

    Like

---
+++ Back-end/src/main/java/com/social/media/platform/entity/Likes.java
@@ -0,0 +1,25 @@
+package com.social.media.platform.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Likes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
