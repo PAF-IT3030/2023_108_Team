4e4f98bb73f93c4fd78d5580f083cabe4bcbcb06
Arsith Subasinghe <100862954+SubasingheIT20299170@users.noreply.github.com>
2023-05-25 05:08:30 +0530

    Commit Comment.java

---
+++ Back-end/src/main/java/com/social/media/platform/entity/Comment.java
@@ -0,0 +1,29 @@
+package com.social.media.platform.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private String comment;
+}
